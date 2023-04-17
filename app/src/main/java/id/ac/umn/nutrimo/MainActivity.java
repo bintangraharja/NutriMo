6a83bad2d809150ba647bc478e83cab951ec9a47
Bintang Raharja <bintang.raharja@student.umn.ac.id>
2023-04-18 01:05:49 +0700

    Auto Slider Artikel

--- app/src/main/java/id/ac/umn/nutrimo/MainActivity.java
+++ app/src/main/java/id/ac/umn/nutrimo/MainActivity.java
@@ -4,11 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageSlider artikelSlider = findViewById(R.id.artikelSlider);
+        ArrayList<SlideModel> artikelModels = new ArrayList<>();
+        artikelModels.add(new SlideModel(R.drawable.artikel_1, ScaleTypes.FIT));
+        artikelModels.add(new SlideModel(R.drawable.artikel_2, ScaleTypes.FIT));
+        artikelModels.add(new SlideModel(R.drawable.artikel_3, ScaleTypes.FIT));
+
+        artikelSlider.setImageList(artikelModels,ScaleTypes.FIT);
     }
 }
